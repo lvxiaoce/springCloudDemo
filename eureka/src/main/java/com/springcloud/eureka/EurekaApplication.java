a0a609616c1acde410e8d4321ee126f5bac0d5f7
lvxiaoce <lvce19950210@163.com>
2018-06-29 16:56:10 +0800

    添加服务注册者eurka

---
+++ eureka/src/main/java/com/springcloud/eureka/EurekaApplication.java
@@ -0,0 +1,14 @@
+package com.springcloud.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer //通过@EnableEurekaServer启动一个服务注册中心给其他应用使用
+@SpringBootApplication
+public class EurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaApplication.class, args);
+	}
+}
